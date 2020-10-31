e841a17f3f979fd027769b99e7a39de5aa1bc161
Sandro6880 <31956997+Sandro6880@users.noreply.github.com>
2020-10-31 14:51:01 +0100

    update: add User foreign key to Membership entity

--- src/main/java/com/audally/backend/entity/Membership.java
+++ src/main/java/com/audally/backend/entity/Membership.java
@@ -2,18 +2,18 @@ package com.audally.backend.entity;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
-import javax.enterprise.inject.Default;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Null;
 import java.util.Date;
 
 @Entity
 @NotNull
 public class Membership extends PanacheEntity {
-
     public MembershipType membershipType;
     public Date subscriptionDate;
     public Date expiryDate;
     public boolean cancelSubscription = false;
+    @ManyToOne
+    public User user;
 }
