051d3d2416cdd79255f15e80e9cd530d8422414b
Sandro6880 <31956997+Sandro6880@users.noreply.github.com>
2020-10-31 14:51:49 +0100

    update: add Lection entity

---
+++ src/main/java/com/audally/backend/entity/Lection.java
@@ -0,0 +1,26 @@
+package com.audally.backend.entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import org.hibernate.validator.constraints.URL;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+import java.sql.Time;
+
+@Entity
+public class Lection extends PanacheEntity {
+    @NotNull
+    public String name;
+    @Max(400)
+    public String description;
+    @URL
+    public String audioUrl;
+
+    public Time duration;
+
+    @ManyToOne
+    public Course course;
+
+}
