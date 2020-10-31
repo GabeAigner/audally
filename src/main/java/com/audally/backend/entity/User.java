1e0e85a1c9ea713b84d3968730d86d3972605db3
Sandro6880 <31956997+Sandro6880@users.noreply.github.com>
2020-10-31 14:50:07 +0100

    update: add courses collection to User entity

--- src/main/java/com/audally/backend/entity/User.java
+++ src/main/java/com/audally/backend/entity/User.java
@@ -4,9 +4,8 @@ package com.audally.backend.entity;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -15,7 +14,6 @@ import java.util.List;
 
 @Entity
 public class User extends PanacheEntity {
-
     @NotNull
     @NotEmpty(message = "Not allowed to be empty!")
     public String userName;
@@ -28,7 +26,9 @@ public class User extends PanacheEntity {
     @NotNull
     public Date joinDate;
     @OneToMany
-    public List<Membership> allMemberships;
-
+    public List<Membership> memberships;
+    @OneToMany
+    @JoinColumn(name = "user_id")
+    public List<Course> courses;
 
 }
