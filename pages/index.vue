72119319c9604c42d4ba6d8d8230b07dc0de355a
gabrielaigner <gabsi.aigner@gmail.com>
2020-11-16 13:18:30 +0100

    update: add loop through CoursePreview pass props

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    <CoursePreview></CoursePreview>
+  <div class="bg-gray-800 h-screen">
+    <ul
+      class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 p-4"
+    >
+      <CoursePreview
+        v-for="course in courses"
+        :key="course.id"
+        :name="course.name"
+        :description="course.description"
+      ></CoursePreview>
+    </ul>
   </div>
 </template>
 
