646975487d497c2fa2946271276f3cb59512053d
gabrielaigner <gabsi.aigner@gmail.com>
2020-11-16 13:26:25 +0100

    update: add CoursesFound? condition in Marketplace

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="bg-gray-800 h-screen">
     <ul
+      v-if="courses.length != 0"
       class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 p-4"
     >
       <CoursePreview
@@ -10,6 +11,13 @@
         :description="course.description"
       ></CoursePreview>
     </ul>
+    <div v-else class="p-4">
+      <li class="flex flex-col text-center bg-white rounded-lg shadow-md p-8">
+        <div class="text-left">
+          <p class="text-gray-700">No courses found...</p>
+        </div>
+      </li>
+    </div>
   </div>
 </template>
 
