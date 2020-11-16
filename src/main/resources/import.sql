594bc07b9d15de3e757ef8da82941142e877ee17
gabrielaigner <gabsi.aigner@gmail.com>
2020-11-16 18:13:24 +0100

    update(Import.sql): add more course test data

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,5 +2,6 @@ insert into audally.users(id, email, password, username) values (1, 'john@doe.co
 insert into audally.users(id, email, password, username) values (2, 'jane@doe.co', 'password', 'jane');
 
 
-insert into audally.courses(id, description, name ) values (1, 'A course about business development.', 'How to Startup?');
-insert into audally.courses(id, description, name ) values (2, 'A memoir by the creator of NIKE', 'Shoe Dog - Phil Knight');
+insert into audally.courses(id, creatoruser, description, name ) values (1, 'A course about business development.', 'How to Startup?');
+insert into audally.courses(id, creatoruser, description, name ) values (2, 'A memoir by the creator of NIKE', 'Shoe Dog - Phil Knight');
+insert into audally.courses(id, creatoruser, description, name ) values (3, 'The course to achieve a healthier and happier life.', 'Start to be happy!');
