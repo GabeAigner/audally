a883ff89a60542db26b7923af4fffb40701a0bbc
gabrielaigner <gabsi.aigner@gmail.com>
2020-11-16 12:22:54 +0100

    update: add browser fetch for courses data

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,19 @@
 </template>
 
 <script>
-export default {}
+export default {
+  async fetch() {
+    await fetch('http://localhost:8080/api/courses')
+      .then((response) => response.json())
+      .then((data) => (this.courses = data))
+  },
+  data() {
+    return {
+      courses: [],
+    }
+  },
+  fetchOnServer: false,
+}
 </script>
 
 <style></style>
