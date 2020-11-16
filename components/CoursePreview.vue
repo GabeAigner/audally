0ad1ffeebcb1e58183c3e4290f56e358e6939eef
gabrielaigner <gabsi.aigner@gmail.com>
2020-11-16 13:19:17 +0100

    update: add CoursePreview   text props visualisation

--- components/CoursePreview.vue
+++ components/CoursePreview.vue
@@ -1,9 +1,19 @@
 <template>
-  <div></div>
+  <li class="flex flex-col text-center bg-white rounded-lg shadow-md p-8">
+    <div class="text-left">
+      <p class="font-medium text-xl">{{ name }}</p>
+      <p class="text-gray-700">{{ description }}</p>
+    </div>
+  </li>
 </template>
 
 <script>
-export default {}
+export default {
+  props: {
+    name: String,
+    description: String,
+  },
+}
 </script>
 
 <style></style>
