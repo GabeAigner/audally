68d41b693bb19609a7c20585a51cba19841cd986
Sandro6880 <31956997+Sandro6880@users.noreply.github.com>
2020-11-17 14:39:25 +0100

    update: add UserResource with addCourse Endpoint.

---
+++ src/main/java/com/audally/backend/boundary/UserResource.java
@@ -0,0 +1,25 @@
+package com.audally.backend.boundary;
+
+import com.audally.backend.entity.Course;
+import com.audally.backend.entity.User;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+import java.lang.module.ResolutionException;
+import java.util.List;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
+@Path("/user")
+@Produces(APPLICATION_JSON)
+public class UserResource {
+    @POST
+    @Path("addCourse/{UserId}-{CourseId}")
+    public Response addCourseToUser(@PathParam("UserId") int Uid,@PathParam("CourseId") int Cid){
+        User user = User.findById(Uid);
+        user.courses.add(Course.findById(Cid));
+        user.persist();
+        return Response.ok(user.toString()).build();
+    }
+}
