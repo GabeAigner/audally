79a5ec48726d3844e341ffc790801a6c0733c429
sandro6880 <sandro6880@gmail.com>
2020-12-01 01:57:00 +0100

    update: remove PanacheEntity extension & replace Time with LocalTime

--- backend-api/src/main/java/com/audally/backend/entity/Lesson.java
+++ backend-api/src/main/java/com/audally/backend/entity/Lesson.java
@@ -3,25 +3,39 @@ package com.audally.backend.entity;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import org.hibernate.validator.constraints.URL;
 
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.json.bind.annotation.JsonbDateFormat;
+import javax.persistence.*;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
 import java.sql.Time;
+import java.time.LocalTime;
 
 @Entity
 @Table(name = "lessons",schema = "audally")
-public class Lesson extends PanacheEntity {
+public class Lesson implements Serializable {
+    @Id
+    @SequenceGenerator(
+            name = "lessonSequence",
+            sequenceName = "lesson_id_seq",
+            initialValue = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+            ,generator = "lessonSequence")
+    private Long id;
     @NotNull
     public String name;
-    @Max(400)
+    @Size(max = 400)
     public String description;
     @URL
     public String audioUrl;
-    public Time duration;
+    @JsonbDateFormat("HH:mm:ss")
+    public LocalTime duration;
 
     @ManyToOne
+    @JoinColumn(name = "course_id")
     public Course course;
 
 }
