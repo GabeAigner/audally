4ae7da2ad376dc72406794311963a9645f085a65
sandro6880 <sandro6880@gmail.com>
2020-12-01 01:55:52 +0100

    update: remove PanacheEntity extension

--- backend-api/src/main/java/com/audally/backend/entity/SubscriptionType.java
+++ backend-api/src/main/java/com/audally/backend/entity/SubscriptionType.java
@@ -2,11 +2,21 @@ package com.audally.backend.entity;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "subscription_types", schema = "audally")
-public class SubscriptionType extends PanacheEntity {
+public class SubscriptionType implements Serializable {
+    @Id
+    @SequenceGenerator(
+            name = "subTypeSequence",
+            sequenceName = "sub_type_id_seq",
+            initialValue = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+            ,generator = "subTypeSequence")
+    private Long id;
     public String name;
 }
