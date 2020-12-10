f2ac91d42980e83def423bf09a8aaaa8bf2cb657
gabrielaigner <gabsi.aigner@gmail.com>
2020-12-10 21:52:32 +0100

    update: Add axios and nuxt auth dependencies

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -31,7 +31,14 @@ export default {
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [],
+  modules: [
+    '@nuxtjs/axios',
+    '@nuxtjs/auth-next'
+  ],
+
+  auth: {
+    // Options
+  },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
