f08a798d77437fb1dd2581658c2518cf294b006c
gabrielaigner <gabsi.aigner@gmail.com>
2020-12-11 14:27:02 +0100

    update: create LoginButton

---
+++ web/components/LoginButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="p-4">
+    <button class="bg-gray-200 p-4 rounded-lg" @click="login">Login</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginVue',
+  auth: 'guest',
+  methods: {
+    async login() {
+      try {
+        const response = await this.$auth.loginWith('keycloak')
+        // eslint-ignore-next-line
+        console.log(response)
+      } catch (err) {
+        // eslint-ignore-next-line
+        console.log(err)
+      }
+    },
+  },
+}
+</script>
