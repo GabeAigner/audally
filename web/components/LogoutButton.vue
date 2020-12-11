d09df8094522e28757b0162a2ff54c2d4f59045a
gabrielaigner <gabsi.aigner@gmail.com>
2020-12-11 15:48:58 +0100

    update: add Logout Button component

---
+++ web/components/LogoutButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="p-4">
+    <button class="bg-gray-400 p-4 rounded-lg" @click="logout">Logout</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    async logout() {
+      try {
+        const response = await this.$auth.logout('keycloak')
+        console.log(response)
+      } catch (err) {
+        console.log(err)
+      }
+    },
+  },
+}
+</script>
