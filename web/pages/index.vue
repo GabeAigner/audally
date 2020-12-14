24837880c2a077f493dd7ca7200cd2c87ac84d60
gabrielaigner <gabsi.aigner@gmail.com>
2020-12-14 16:15:27 +0100

    update: remove courses from  index page local data

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -2,8 +2,7 @@
   <div class="bg-gray-800 min-h-screen">
     <ButtonBar></ButtonBar>
     <FeaturedCourses
-      v-if="courses.length != 0"
-      :courses="courses"
+      v-if="this.$store.state.courses.length != 0"
     ></FeaturedCourses>
     <div v-else class="p-4">
       <CoursePreviewSkeleton></CoursePreviewSkeleton>
@@ -16,8 +15,7 @@
       </li>-->
     </div>
     <PersonalCourses
-      v-if="personalCourses != 0"
-      :courses="personalCourses"
+      v-if="this.$store.state.personalCourses.length != 0"
     ></PersonalCourses>
   </div>
 </template>
@@ -28,6 +26,7 @@ export default {
     await fetch('http://localhost:8080/api/courses')
       .then((response) => response.json())
       .then((data) => (this.courses = data))
+    this.$store.commit('setCourses', this.courses)
     if (this.$auth.loggedIn) {
       let user
       await fetch('http://localhost:8080/api/users/1') //  + this.$auth.user.email
@@ -37,13 +36,8 @@ export default {
         await fetch('http://localhost:8080/api/users/' + user.id + '/courses')
           .then((response) => response.json())
           .then((data) => (this.personalCourses = data))
+        this.$store.commit('setPersonalCourses', this.personalCourses)
       }
-    }
-  },
-  data() {
-    return {
-      courses: [],
-      personalCourses: [],
     }
   },
   fetchOnServer: false,
