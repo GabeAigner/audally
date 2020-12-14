63eaa06b94e12460ba0035298a0feadddba5a673
gabrielaigner <gabsi.aigner@gmail.com>
2020-12-14 16:12:40 +0100

    update: add courses to the index  store

--- web/store/index.js
+++ web/store/index.js
@@ -1,3 +1,19 @@
-export const state = () => ({})
+export const state = () => ({
+  courses: [],
+  personalCourses: [],
+})
 
-export const mutations = {}
+export const mutations = {
+  addCourse(state, course) {
+    state.courses.push(course)
+  },
+  addPersonalCourse(state, course) {
+    state.personalCourses.push(course)
+  },
+  setCourses(state, courses) {
+    state.courses = courses
+  },
+  setPersonalCourses(state, courses) {
+    state.personalCourses = courses
+  },
+}
