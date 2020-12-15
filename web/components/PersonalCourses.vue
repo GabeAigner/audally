706da0ff5ad6ed3114c9eea9fc47259eb40ffc41
gabrielaigner <gabsi.aigner@gmail.com>
2020-12-15 11:03:07 +0100

    update: pass userid to personalCourses.vue

--- web/components/PersonalCourses.vue
+++ web/components/PersonalCourses.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <h1 class="px-4 text-3xl font-bold text-gray-300">Personal Courses</h1>
+    <h1 class="px-4 mt-8 text-3xl font-bold text-gray-300 sm:mb-0">
+      Personal Courses
+    </h1>
     <ul
       class="grid grid-cols-1 gap-6 sm:p-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
     >
@@ -8,6 +10,7 @@
         v-for="course in courses"
         :key="course.id"
         :course="course"
+        :userid="userid"
       ></CoursePreview>
     </ul>
   </div>
@@ -15,6 +18,12 @@
 
 <script>
 export default {
+  props: {
+    userid: {
+      type: Number,
+      default: -1,
+    },
+  },
   computed: {
     courses() {
       return this.$store.state.personalCourses
