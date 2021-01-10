86849cf423a6e7916da811d82a2d41cdfc5f9757
gabrielaigner <gabsi.aigner@gmail.com>
2021-01-10 22:18:23 +0100

    update: add PageHeader to courses and explore page

--- web/pages/courses.vue
+++ web/pages/courses.vue
@@ -1,5 +1,7 @@
 <template>
-  <div></div>
+  <div>
+    <PageHeader name="My Courses"></PageHeader>
+  </div>
 </template>
 
 <script>
--- web/pages/explore.vue
+++ web/pages/explore.vue
@@ -1,5 +1,7 @@
 <template>
-  <div></div>
+  <div>
+    <PageHeader name="Explore"></PageHeader>
+  </div>
 </template>
 
 <script>
