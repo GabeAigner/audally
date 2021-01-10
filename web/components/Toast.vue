dab1c6f697130f4c55519d1a75c314528cbab47b
gabrielaigner <gabsi.aigner@gmail.com>
2021-01-10 21:38:40 +0100

    update(Toast): add transitions and update text

--- web/components/Toast.vue
+++ web/components/Toast.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- This example requires Tailwind CSS v2.0+ -->
   <div
-    class="fixed inset-0 flex items-end justify-center px-4 py-6 pointer-events-none sm:p-6 sm:items-start sm:justify-end"
+    class="fixed inset-0 z-20 flex items-end justify-center px-4 py-6 pointer-events-none sm:p-6 lg:items-start sm:justify-end"
   >
     <!--
     Notification panel, show/hide based on alert state.
@@ -13,65 +13,137 @@
       From: "opacity-100"
       To: "opacity-0"
   -->
-    <div
-      class="max-w-sm w-full bg-white shadow-lg rounded-lg pointer-events-auto ring-1 ring-black ring-opacity-5 overflow-hidden"
+    <transition
+      enter-active-class="transition duration-300 ease-out transform"
+      enter-class="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+      enter-to-class="translate-y-0 opacity-100 sm:translate-x-0"
+      leave-active-class="transition duration-100 ease-in"
+      leave-class="opacity-100"
+      leave-to-class="opacity-0"
     >
-      <div class="p-4">
-        <div class="flex items-start">
-          <div class="flex-shrink-0">
-            <!-- Heroicon name: check-circle -->
-            <svg
-              class="h-6 w-6 text-green-400"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-          </div>
-          <div class="ml-3 w-0 flex-1 pt-0.5">
-            <p class="text-sm font-medium text-gray-900">Successfully added!</p>
-            <p class="mt-1 text-sm text-gray-500">
-              Your course progress will now be saved.
-            </p>
-          </div>
-          <div class="ml-4 flex-shrink-0 flex">
-            <button
-              class="bg-white rounded-md inline-flex text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              @click="toggleToast"
-            >
-              <span class="sr-only">Close</span>
-              <!-- Heroicon name: x -->
+      <div
+        v-show="show"
+        class="z-20 w-full max-w-sm overflow-hidden bg-gray-200 rounded-lg shadow-lg pointer-events-auto ring-1 ring-black ring-opacity-5"
+      >
+        <div v-if="positive" class="p-4">
+          <div class="flex items-start">
+            <div class="flex-shrink-0">
+              <!-- Heroicon name: check-circle -->
               <svg
-                class="h-5 w-5"
+                class="w-6 h-6 text-green-400"
                 xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
                 aria-hidden="true"
               >
                 <path
-                  fill-rule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
                 />
               </svg>
-            </button>
+            </div>
+            <div class="ml-3 w-0 flex-1 pt-0.5">
+              <p class="text-sm font-medium text-gray-900">Course saved!</p>
+              <p class="mt-1 text-sm text-gray-600">
+                Your progress for this course will now be saved. See it in
+                <nuxt-link to="/courses" class="underline">My Courses</nuxt-link
+                >.
+              </p>
+            </div>
+            <div class="flex flex-shrink-0 ml-4">
+              <button
+                class="inline-flex text-gray-500 bg-gray-200 rounded-md hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                @click="toggleToast"
+              >
+                <span class="sr-only">Close</span>
+                <!-- Heroicon name: x -->
+                <svg
+                  class="w-5 h-5"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div v-if="!positive" class="p-4">
+          <div class="flex items-start">
+            <div class="flex-shrink-0">
+              <!-- Heroicon name: check-circle -->
+              <svg
+                class="w-6 h-6 text-red-400"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                ></path>
+              </svg>
+            </div>
+            <div class="ml-3 w-0 flex-1 pt-0.5">
+              <p class="text-sm font-medium text-gray-900">Course removed!</p>
+              <p class="mt-1 text-sm text-gray-600">
+                Your progress for this course has been deleted and is no longer
+                saved.
+              </p>
+            </div>
+            <div class="flex flex-shrink-0 ml-4">
+              <button
+                class="inline-flex text-gray-500 bg-gray-200 rounded-md hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                @click="toggleToast"
+              >
+                <span class="sr-only">Close</span>
+                <!-- Heroicon name: x -->
+                <svg
+                  class="w-5 h-5"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </transition>
   </div>
 </template>
 
 <script>
 export default {
+  props: {
+    show: {
+      type: Boolean,
+      default: false,
+    },
+    positive: {
+      type: Boolean,
+      default: false,
+    },
+  },
   methods: {
     toggleToast() {
       this.$emit('toggleToast')
