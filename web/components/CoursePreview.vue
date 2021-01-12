4ba3984bf717e2655b16d79131a6a8f93bf322bf
gabrielaigner <gabsi.aigner@gmail.com>
2021-01-12 09:46:55 +0100

    update(CoursePreview): buttons and emits

--- web/components/CoursePreview.vue
+++ web/components/CoursePreview.vue
@@ -1,60 +1,89 @@
 <template>
   <li
-    class="bg-center bg-cover rounded-lg hover:shadow-xl duration-150 ease-in-out"
+    class="duration-150 ease-in-out bg-center bg-cover rounded-lg hover:shadow-xl"
     :style="{ 'background-image': `url(${course.pictureUrl})` }"
     @click="toDetail"
   >
-    <div class="h-24 bg-gray-800 bg-opacity-50 rounded-t-lg">
-      <div class="inline-block float-right p-4">
-        <button
-          v-if="listtype === 'featured' && this.$auth.loggedIn"
-          class="text-gray-400 rounded-full focus:outline-none hover:text-white"
-          @click.stop="addCourse"
-        >
-          <svg
-            class="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-            ></path>
-          </svg>
-        </button>
-        <button
-          v-if="listtype !== 'featured'"
-          class="inline-block text-gray-400 rounded-full focus:outline-none"
-          @click.stop="removeCourse"
-        >
-          <svg
-            class="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-            ></path>
-          </svg>
-        </button>
-      </div>
-      <!-- Thumbnail image -->
-    </div>
     <div
-      class="flex flex-col p-8 text-center bg-gray-800 bg-opacity-50 rounded-b-lg shadow-md"
+      class="h-full bg-gray-800 bg-opacity-50 rounded-t-lg rounded-b-lg shadow-md"
     >
-      <div class="text-left">
-        <p class="text-xl font-medium text-white">{{ course.name }}</p>
-        <p class="text-gray-200">{{ course.description }}</p>
+      <div class="h-24 rounded-t-lg">
+        <div class="inline-block float-right p-2">
+          <button
+            v-if="
+              listtype === 'featured' &&
+              this.$auth.loggedIn &&
+              isDisabled() === false
+            "
+            class="p-2 text-gray-400 bg-gray-800 rounded-full focus:outline-none hover:text-white"
+            @click.stop="addCourse"
+          >
+            <svg
+              class="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+              ></path>
+            </svg>
+          </button>
+          <div
+            v-if="
+              listtype === 'featured' &&
+              this.$auth.loggedIn &&
+              isDisabled() === true
+            "
+            class="p-2 text-blue-400 bg-gray-800 rounded-full focus:outline-none"
+          >
+            <svg
+              class="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M5 13l4 4L19 7"
+              ></path>
+            </svg>
+          </div>
+          <button
+            v-if="listtype !== 'featured'"
+            class="inline-block p-2 text-gray-400 bg-gray-800 rounded-full focus:outline-none"
+            @click.stop="removeCourse"
+          >
+            <svg
+              class="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+              ></path>
+            </svg>
+          </button>
+        </div>
+        <!-- Thumbnail image -->
+      </div>
+      <div class="flex flex-col p-8 text-center rounded-b-lg">
+        <div class="text-left">
+          <p class="text-xl font-medium text-white">{{ course.name }}</p>
+          <p class="text-gray-200">{{ course.description }}</p>
+        </div>
       </div>
     </div>
   </li>
@@ -73,14 +102,7 @@ export default {
     },
     course: {
       type: Object,
-      default() {
-        return {
-          id: 1,
-          name: 'Default',
-          lessons: [],
-          description: 'Default',
-        }
-      },
+      default: null,
     },
   },
   computed: {
@@ -89,12 +111,21 @@ export default {
     },
   },
   methods: {
+    isDisabled() {
+      if (this.courses.some((c) => c.id === this.course.id)) {
+        console.log(this.course.name + ' is disabled')
+
+        return true
+      }
+      console.log(this.course.name + ' is NOT disabled')
+      return false
+    },
     toDetail() {
       if (this.$auth.loggedIn === null || this.$auth.loggedIn === false) {
         return
       }
       this.$store.commit('updateCourse', this.course)
-      this.$router.push('CourseView')
+      this.$router.push('course/' + this.course.id)
     },
     async addCourse(e) {
       if (this.$auth.loggedIn === null || this.$auth.loggedIn === false) {
@@ -106,6 +137,7 @@ export default {
           console.log('exists')
           return
         }
+        this.$emit('addCourse')
         console.log('goes into it though')
         this.$store.commit('addPersonalCourse', this.course)
         await fetch(
