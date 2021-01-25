b39fc2f6ede2e581b212327e2876aa491330759d
gabrielaigner <gabsi.aigner@gmail.com>
2021-01-25 23:36:48 +0100

    update(course): add lessons with play button

--- web/pages/course/_id.vue
+++ web/pages/course/_id.vue
@@ -1,216 +1,145 @@
 <template>
   <div class="h-screen bg-gray-800">
     <PageHeader :name="course.name" :back="true"></PageHeader>
-    <div
-      class="w-full h-64 bg-center bg-cover rounded-lg"
-      :style="{ 'background-image': `url(${course.pictureUrl})` }"
-    ></div>
-    <div class="p-4 -mt-8 bg-gray-800 rounded-2xl">
-      <h1 class="text-2xl font-bold tracking-wider text-white">
-        {{ course.name }}
-      </h1>
-      <p class="mt-4 text-lg tracking-wider text-gray-300">
-        {{ course.description }}
-      </p>
+    <div class="items-center justify-between hidden w-full lg:flex">
+      <div
+        class="flex flex-col items-start w-full p-4 rounded lg:p-8 lg:flex-row lg:items-center"
+      >
+        <div class="w-full pt-4 lg:pt-0 lg:w-2/3">
+          <h1
+            class="hidden text-4xl font-bold tracking-wider text-white lg:block"
+          >
+            {{ course.name }}
+          </h1>
+          <p class="mt-4 text-xl tracking-wider text-gray-300">
+            {{ course.description }}
+          </p>
+          <p class="mt-4 text-lg tracking-wider text-gray-500">
+            Lessons:
+            {{ course.lessons.length }}
+          </p>
+        </div>
+        <div
+          :style="{ 'background-image': `url(${course.pictureUrl})` }"
+          class="w-full h-24 bg-center bg-cover rounded-lg lg:w-1/3 lg:h-64"
+        ></div>
+      </div>
     </div>
-    <div>
-      <!-- This example requires Tailwind CSS v2.0+ -->
-      <div class="p-4">
-        <h2 class="text-xs font-medium tracking-wide text-gray-500 uppercase">
+
+    <div class="flex items-center justify-between w-full lg:hidden">
+      <div
+        class="flex flex-col items-start w-full p-4 rounded lg:p-8 lg:flex-row lg:items-center"
+      >
+        <div
+          :style="{ 'background-image': `url(${course.pictureUrl})` }"
+          class="w-full h-24 bg-center bg-cover rounded-lg lg:w-1/3 lg:h-64"
+        ></div>
+        <div class="w-full pt-4 lg:pt-0 lg:pl-8 lg:w-2/3">
+          <h1
+            class="hidden text-4xl font-bold tracking-wider text-white lg:block"
+          >
+            {{ course.name }}
+          </h1>
+          <p class="mt-4 text-xl tracking-wider text-gray-300">
+            {{ course.description }}
+          </p>
+          <p class="mt-4 text-lg tracking-wider text-gray-500">
+            Lessons:
+            {{ course.lessons.length }}
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <!-- This example requires Tailwind CSS v2.0+ -->
+    <div class="h-20 px-4 py-4 mx-auto max-w-7xl lg:px-8">
+      <!-- We've used 3xl here, but feel free to try other max-widths based on your needs -->
+      <div class="h-20 max-w-4xl">
+        <h2 class="font-medium tracking-wide text-gray-500 uppercase text-md">
           Course Lessons
         </h2>
-        <ul
-          class="grid grid-cols-1 gap-5 mt-3 sm:gap-6 sm:grid-cols-2 lg:grid-cols-4"
-        >
-          <li class="flex col-span-1 rounded-md shadow-sm">
+        <ul class="">
+          <li
+            v-for="(lesson, i) in course.lessons"
+            :key="i"
+            class="flex col-span-1 mt-3 rounded-md shadow-sm"
+          >
             <div
-              class="flex items-center justify-center flex-shrink-0 text-2xl font-medium text-white bg-gray-700 w-13 rounded-l-md"
+              class="flex items-center justify-center flex-shrink-0 p-4 text-2xl font-medium text-white bg-gray-700 w-13 rounded-l-md"
             >
-              1.
+              {{ i + 1 }}.
             </div>
             <div
               class="flex items-center justify-between flex-1 truncate bg-gray-700 rounded-r-md"
             >
-              <div class="flex-1 px-4 py-2 text-sm truncate">
-                <a
-                  href="#"
-                  class="font-medium text-gray-900 hover:text-gray-600"
-                  >Graph API</a
-                >
-                <p class="text-gray-500">16 Members</p>
+              <div class="flex-1 px-4 py-2 truncate text-md">
+                <a href="#" class="font-medium text-white hover:text-gray-300">
+                  {{ lesson.name }}
+                </a>
+                <p class="hidden text-sm text-gray-500 lg:block">
+                  {{ lesson.description }}
+                </p>
+                <p class="block text-sm text-gray-500 lg:hidden">
+                  {{ lesson.duration }}
+                </p>
               </div>
-              <div class="flex-shrink-0 pr-2">
+              <div
+                class="flex items-center justify-center flex-shrink-0 p-4 text-gray-400"
+              >
+                <p class="hidden mr-4 lg:block">
+                  {{ lesson.duration }}
+                </p>
                 <button
-                  class="inline-flex items-center justify-center w-8 h-8 text-gray-400 bg-transparent bg-white rounded-full hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  v-if="!isPlaying(lesson)"
+                  class="inline-flex items-center justify-center w-8 h-8 text-gray-400 bg-transparent rounded-full hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  @click="play(lesson)"
                 >
                   <span class="sr-only">Open options</span>
                   <!-- Heroicon name: dots-vertical -->
                   <svg
-                    class="w-5 h-5"
+                    class="w-8 h-8"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
                     xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    aria-hidden="true"
                   >
                     <path
-                      d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z"
-                    />
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
+                    ></path>
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    ></path>
                   </svg>
                 </button>
-              </div>
-            </div>
-          </li>
-
-          <li class="flex col-span-1 rounded-md shadow-sm">
-            <div
-              class="flex items-center justify-center flex-shrink-0 w-16 text-sm font-medium text-white bg-purple-600 rounded-l-md"
-            >
-              CD
-            </div>
-            <div
-              class="flex items-center justify-between flex-1 truncate bg-white border-t border-b border-r border-gray-200 rounded-r-md"
-            >
-              <div class="flex-1 px-4 py-2 text-sm truncate">
-                <a
-                  href="#"
-                  class="font-medium text-gray-900 hover:text-gray-600"
-                  >Component Design</a
-                >
-                <p class="text-gray-500">12 Members</p>
-              </div>
-              <div class="flex-shrink-0 pr-2">
                 <button
-                  class="inline-flex items-center justify-center w-8 h-8 text-gray-400 bg-transparent bg-white rounded-full hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  v-if="isPlaying(lesson)"
+                  class="inline-flex items-center justify-center w-8 h-8 text-gray-400 bg-transparent rounded-full hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  @click="stop(lesson)"
                 >
                   <span class="sr-only">Open options</span>
                   <!-- Heroicon name: dots-vertical -->
                   <svg
-                    class="w-5 h-5"
+                    class="w-8 h-8"
+                    :class="
+                      isPlaying(lesson) ? 'animate-spin duration-2000' : ''
+                    "
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
                     xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    aria-hidden="true"
                   >
                     <path
-                      d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z"
-                    />
-                  </svg>
-                </button>
-              </div>
-            </div>
-          </li>
-
-          <li class="flex col-span-1 rounded-md shadow-sm">
-            <div
-              class="flex items-center justify-center flex-shrink-0 w-16 text-sm font-medium text-white bg-yellow-500 rounded-l-md"
-            >
-              T
-            </div>
-            <div
-              class="flex items-center justify-between flex-1 truncate bg-white border-t border-b border-r border-gray-200 rounded-r-md"
-            >
-              <div class="flex-1 px-4 py-2 text-sm truncate">
-                <a
-                  href="#"
-                  class="font-medium text-gray-900 hover:text-gray-600"
-                  >Templates</a
-                >
-                <p class="text-gray-500">16 Members</p>
-              </div>
-              <div class="flex-shrink-0 pr-2">
-                <button
-                  class="inline-flex items-center justify-center w-8 h-8 text-gray-400 bg-transparent bg-white rounded-full hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                  <span class="sr-only">Open options</span>
-                  <!-- Heroicon name: dots-vertical -->
-                  <svg
-                    class="w-5 h-5"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z"
-                    />
-                  </svg>
-                </button>
-              </div>
-            </div>
-          </li>
-
-          <li class="flex col-span-1 rounded-md shadow-sm">
-            <div
-              class="flex items-center justify-center flex-shrink-0 w-16 text-sm font-medium text-white bg-green-500 rounded-l-md"
-            >
-              RC
-            </div>
-            <div
-              class="flex items-center justify-between flex-1 truncate bg-white border-t border-b border-r border-gray-200 rounded-r-md"
-            >
-              <div class="flex-1 px-4 py-2 text-sm truncate">
-                <a
-                  href="#"
-                  class="font-medium text-gray-900 hover:text-gray-600"
-                  >React Components</a
-                >
-                <p class="text-gray-500">8 Members</p>
-              </div>
-              <div class="flex-shrink-0 pr-2">
-                <button
-                  class="inline-flex items-center justify-center w-8 h-8 text-gray-400 bg-transparent bg-white rounded-full hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                  <span class="sr-only">Open options</span>
-                  <!-- Heroicon name: dots-vertical -->
-                  <svg
-                    class="w-5 h-5"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z"
-                    />
-                  </svg>
-                </button>
-              </div>
-            </div>
-          </li>
-
-          <li class="flex col-span-1 rounded-md shadow-sm">
-            <div
-              class="flex items-center justify-center flex-shrink-0 w-16 text-sm font-medium text-white bg-green-500 rounded-l-md"
-            >
-              RC
-            </div>
-            <div
-              class="flex items-center justify-between flex-1 truncate bg-white border-t border-b border-r border-gray-200 rounded-r-md"
-            >
-              <div class="flex-1 px-4 py-2 text-sm truncate">
-                <a
-                  href="#"
-                  class="font-medium text-gray-900 hover:text-gray-600"
-                  >React Components</a
-                >
-                <p class="text-gray-500">8 Members</p>
-              </div>
-              <div class="flex-shrink-0 pr-2">
-                <button
-                  class="inline-flex items-center justify-center w-8 h-8 text-gray-400 bg-transparent bg-white rounded-full hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                  <span class="sr-only">Open options</span>
-                  <!-- Heroicon name: dots-vertical -->
-                  <svg
-                    class="w-5 h-5"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z"
-                    />
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z"
+                    ></path>
                   </svg>
                 </button>
               </div>
@@ -219,7 +148,15 @@
         </ul>
       </div>
     </div>
-    <span class="amplitude-play"></span>
+    <div
+      class="fixed bottom-0 w-full h-20 bg-white border-2 border-blue-400 left-64"
+    >
+      <vue-plyr class="relative">
+        <audio controls crossorigin playsinline preload="auto">
+          <source id="plyr" :src="current" type="audio/mp3" />
+        </audio>
+      </vue-plyr>
+    </div>
   </div>
 </template>
 
@@ -229,12 +166,42 @@ export default {
   data() {
     return {
       course: '',
+      player: null,
+      playing: false,
+      playingLesson: '',
+      currentAudioUrl: '',
     }
   },
   created() {
     this.course = this.$store.state.currentCourse
   },
+  mounted() {
+    this.player = document.getElementById('myAudio')
+  },
+  methods: {
+    isPlaying(lesson) {
+      if (this.playingLesson === lesson) {
+        if (this.playing === true) return true
+      }
+      return false
+    },
+    play(lesson) {
+      this.playingLesson = lesson
+      this.playing = true
+      this.currentAudioUrl = lesson.audioUrl
+      this.player.play()
+    },
+    stop(lesson) {
+      this.playingLesson = ''
+      this.playing = false
+      this.player.pause()
+    },
+  },
 }
 </script>
 
-<style></style>
+<style>
+.left-64 {
+  left: 16rem;
+}
+</style>
