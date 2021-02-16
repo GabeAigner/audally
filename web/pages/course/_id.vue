edab7809c073eaabe16155117266931093ad82ca
gabrielaigner <gabsi.aigner@gmail.com>
2021-02-16 15:01:33 +0100

    update(course): add alreadyListened badge

--- web/pages/course/_id.vue
+++ web/pages/course/_id.vue
@@ -88,6 +88,24 @@
               <div
                 class="flex items-center justify-center flex-shrink-0 p-4 text-gray-400"
               >
+                <svg
+                  v-if="
+                    progresses.find((p) => p.lesson.id === lesson.id)
+                      .alreadyListened === true
+                  "
+                  class="w-6 h-6 text-green-500 mr-4"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"
+                  ></path>
+                </svg>
                 <p class="hidden mr-4 lg:block">
                   {{ lesson.duration }}
                 </p>
@@ -128,6 +146,17 @@
 
 <script>
 export default {
+  async fetch() {
+    await fetch(
+      'http://localhost:8080/api/users/' +
+        this.user.id +
+        '/courses/' +
+        this.course.id +
+        '/progresses'
+    )
+      .then((response) => response.json())
+      .then((data) => this.$store.commit('updateProgresses', data))
+  }, // http://localhost:8080/api/users/1/courses/1/progresses
   layout: 'next',
   data() {
     return {
@@ -141,8 +170,14 @@ export default {
     course() {
       return this.$store.state.currentCourse
     },
+    user() {
+      return this.$store.state.user
+    },
     clesson() {
       return this.$store.state.lesson === null ? {} : this.$store.state.lesson
+    },
+    progresses() {
+      return this.$store.state.progresses
     },
   },
   mounted() {
@@ -151,6 +186,10 @@ export default {
   methods: {
     play(lesson) {
       this.$store.commit('updateLesson', lesson)
+      this.$store.commit(
+        'updateProgress',
+        this.progresses.find((s) => s.lesson.id === lesson.id)
+      )
       this.$nuxt.$emit('updateAudio')
     },
   },
