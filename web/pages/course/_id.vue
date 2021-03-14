980286f78712ce6971dbcbdc0715f44e98cbbd3b
gabrielaigner <gabsi.aigner@gmail.com>
2021-03-14 19:18:18 +0100

    update(course): fixed alreadyListened undefined bug

--- web/pages/course/_id.vue
+++ web/pages/course/_id.vue
@@ -86,26 +86,31 @@
                 </p>
               </div>
               <div
+                v-if="$auth.loggedIn"
                 class="flex items-center justify-center flex-shrink-0 p-4 text-gray-400"
               >
-                <svg
-                  v-if="
-                    progresses.find((p) => p.lesson.id === lesson.id)
-                      .alreadyListened === true
-                  "
-                  class="w-6 h-6 text-green-500 mr-4"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
+                <template
+                  v-if="progresses.find((p) => p.lesson.id === lesson.id)"
                 >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"
-                  ></path>
-                </svg>
+                  <svg
+                    v-if="
+                      progresses.find((p) => p.lesson.id === lesson.id)
+                        .alreadyListened === true
+                    "
+                    class="w-6 h-6 text-green-500 mr-4"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"
+                    ></path>
+                  </svg>
+                </template>
                 <p class="hidden mr-4 lg:block">
                   {{ lesson.duration }}
                 </p>
